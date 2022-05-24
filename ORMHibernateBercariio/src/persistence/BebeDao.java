fb0ea3022c872855ed4263a15e7c7f5be3cdb541
lucas.henrique2@fatec.sp.gov.br <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>
2022-05-23 23:23:38 -0300

    Hibernate Persistance

---
+++ ORMHibernateBercariio/src/persistence/BebeDao.java
@@ -0,0 +1,40 @@
+package persistence;
+
+import java.util.List;
+
+import model.Bebe;
+
+public class BebeDao implements IObjDao<Bebe>{
+
+	@Override
+	public void insere(Bebe t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modifica(Bebe t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remove(Bebe t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Bebe busca(Bebe t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Bebe> lista() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
---
+++ ORMHibernateBercariio/src/persistence/MaeDao.java
@@ -0,0 +1,40 @@
+package persistence;
+
+import java.util.List;
+
+import model.Mae;
+
+public class MaeDao implements IObjDao<Mae> {
+
+	@Override
+	public void insere(Mae t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modifica(Mae t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void remove(Mae t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Mae busca(Mae t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Mae> lista() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
